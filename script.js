a7ddffd454e6fc308a18694ff685740fea513a63
jlhelwig <jeff.helwig@gmail.com>
2018-04-12 09:20:46 -0500

    passes items and makes an array

---
+++ script.js
@@ -0,0 +1,14 @@
+// a friend said he had a whiteboard interview question:
+//'create a function that check two words, see if they are anagrams'
+//my initial reaction was: I think I could do that!
+
+
+function myFunction(aide,idea){
+//first sort them, then compare them... first make them an array
+let done=aide.split('');
+let node=idea.split('');
+// check it out
+console.log(done,node);
+}
+
+myFunction('notes', 'snotes');
